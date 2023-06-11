2338dee2d6c22a1582276be48b001af9a961e6f8
Timurlan <126976626+timurlan05@users.noreply.github.com>
2023-06-11 14:09:51 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,24 +1,64 @@
+import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
+    @pytest.fixture
+    def collector(self):
+        return BooksCollector()
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    def test_add_new_book(self, collector):
+        collector.add_new_book("Book 1")
+        assert collector.books_rating == {"Book 1": 1}
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    def test_add_new_book_duplicate(self, collector):
+        collector.add_new_book("Book 1")
+        collector.add_new_book("Book 1")
+        assert collector.books_rating == {"Book 1": 1}
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_set_book_rating(self, collector):
+        collector.add_new_book("Book 1")
+        collector.set_book_rating("Book 1", 5)
+        assert collector.books_rating == {"Book 1": 5}
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_set_book_rating_invalid_rating(self, collector):
+        collector.add_new_book("Book 1")
+        collector.set_book_rating("Book 1", 15)
+        assert collector.books_rating == {"Book 1": 1}
+
+    def test_get_book_rating(self, collector):
+        collector.add_new_book("Book 1")
+        collector.set_book_rating("Book 1", 5)
+        assert collector.get_book_rating("Book 1") == 5
+
+    def test_get_books_with_specific_rating(self, collector):
+        collector.add_new_book("Book 1")
+        collector.add_new_book("Book 2")
+        collector.add_new_book("Book 3")
+        collector.set_book_rating("Book 1", 5)
+        collector.set_book_rating("Book 2", 3)
+        collector.set_book_rating("Book 3", 5)
+        assert collector.get_books_with_specific_rating(5) == ["Book 1", "Book 3"]
+
+    def test_get_books_rating(self, collector):
+        collector.add_new_book("Book 1")
+        collector.add_new_book("Book 2")
+        collector.add_new_book("Book 3")
+        collector.set_book_rating("Book 1", 5)
+        collector.set_book_rating("Book 2", 3)
+        collector.set_book_rating("Book 3", 5)
+        assert collector.get_books_rating() == {"Book 1": 5, "Book 2": 3, "Book 3": 5}
+
+    def test_add_book_in_favorites(self, collector):
+        collector.add_new_book("Book 1")
+        collector.add_book_in_favorites("Book 1")
+        assert collector.favorites == ["Book 1"]
+
+    def test_add_book_in_favorites_invalid_book(self, collector):
+        collector.add_book_in_favorites("Book 1")
+        assert collector.favorites == []
+
+    def test_delete_book_from_favorites(self, collector):
+        collector.add_new_book("Book 1")
+        collector.add_book_in_favorites("Book 1")
+        collector.delete_book_from_favorites("Book 1")
+        assert collector.favorites == []
